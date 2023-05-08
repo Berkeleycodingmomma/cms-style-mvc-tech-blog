810980773ce7f65b583fc85e6cffa26e4f78e867
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-08 17:47:26 -0600

    figuring out connection

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,12 @@
 
-// require('dotenv').config(); // Importing sensitive data from .env
-
-// const Sequelize = require('sequelize');
-
-
-// const sequelize = process.env.JAWSDB_URL 
-//   ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
-
-// module.exports = sequelize;
-
-
-require('dotenv').config();
+require('dotenv').config(); // Importing sensitive data from .env
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+
+const sequelize = process.env.JAWSDB_URL 
+  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
@@ -32,3 +15,20 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+
+// require('dotenv').config();
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
