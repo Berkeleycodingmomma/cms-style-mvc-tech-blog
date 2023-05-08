cefb910d824fe86418085fb0d887d34899213add
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-08 00:07:00 -0600

    inporting seed data functions and  sequilize connection

--- seeds/index.js
+++ seeds/index.js
@@ -7,5 +7,17 @@ const seedComments = require("./commentData");
 const sequelize = require('../config/connection');
 
 const seedAll  = async () => {
-    
-}
+
+await sequelize.sync({ force: true });
+
+// Calling each of the seed data functions
+await seedUsers();
+await seedPosts();
+await seedComments();
+
+// Exiting the process with a successful exit code
+process.exit(0);
+};
+
+// Calling the seedAll function to seed the database
+seedAll();
