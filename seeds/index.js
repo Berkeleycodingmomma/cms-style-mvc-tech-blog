d27c4ea072730371f7413dbea2e483dc78e026f0
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-07 23:49:44 -0600

    starting the index.js in seeds

---
+++ seeds/index.js
@@ -0,0 +1,11 @@
+
+//Below importing the seed data functions 
+const seedUsers = require("./userData");
+const seedPosts = require("./postData");
+const seedComments = require("./commentData");
+
+const sequelize = require('../config/connection');
+
+const seedAll  = async () => {
+    
+}
