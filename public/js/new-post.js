444941750ca99af9f1600f0ea5d2b1322fca4cf0
Amanda Gray <floridabelle2021@protonmail.com>
2023-05-08 23:05:14 -0600

    still debugging

--- public/js/new-post.js
+++ public/js/new-post.js
@@ -1,11 +1,40 @@
+// const newEquinePostFormHandler = async (event) => {
+//     event.preventDefault();
+  
+//     let title = '';
+//     const titleInput = document.querySelector('#title-new-equine-post');
+//     if (titleInput) {
+//       title = titleInput.value.trim();
+//     }
+  
+//     const content = document.querySelector('#content-new-equine-post').value.trim();
+  
+//     if (title && content) {
+//       const response = await fetch('/api/posts', {
+//         method: 'POST',
+//         body: JSON.stringify({ title, content }),
+//         headers: { 'Content-Type': 'application/json' },
+//       });
+  
+//       if (response.ok) {
+//         document.location.replace('/dashboard'); // When successful, load the dashboard page
+//       } else {
+//         alert('Failed to create a new post.'); // When unsuccessful, show alert
+//       }
+//     }
+//   };
+  
 
-//Below creating a new post 
 const newEquinePostFormHandler = async (event) => {
     event.preventDefault();
   
+    let title = '';
     const titleInput = document.querySelector('#title-new-equine-post');
-    // const title = document.querySelector('#title-new-equine-post').value.trim();
-    const contentInput = document.querySelector('#content-new-equine-post');
+    if (titleInput) {
+      title = titleInput.value.trim();
+    }
+  
+    const content = document.querySelector('#content-new-equine-post').value.trim();
   
     if (title && content) {
       const response = await fetch('/api/posts', {
@@ -21,6 +50,7 @@ const newEquinePostFormHandler = async (event) => {
       }
     }
   };
+  
   
   // Event listeners
   const newEquinePostForm = document.querySelector('.new-equine-post-form');
